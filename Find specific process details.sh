2001b9eb26700cf464b116db58fd064c5f63bddf
PIN_NAKA <153271169+CHANDRAHAS-SHIVA@users.noreply.github.com>
2026-04-19 21:44:07 +0530

    Update Find specific process details.sh

--- Find specific process details.sh
+++ Find specific process details.sh
@@ -2,3 +2,9 @@
 #!/bin/bash
 #
 # this to find a specific result from process
+# Creator - Rabijit
+# Date - 19/04/2026
+##########################################
+set -x
+ps -ef | grep "sshd" | awk -F" " '{print $2}'
+# it will find all processes with ps -ef,, grep will find a specific line(row) or statement and awk will print the column
