389a2a656de057e829e42df0a18c3fbbfcf1fc4f
PIN_NAKA <153271169+CHANDRAHAS-SHIVA@users.noreply.github.com>
2026-04-20 12:42:09 +0530

    Update findprocessdetail.sh

--- findprocessdetail.sh
+++ findprocessdetail.sh
@@ -5,6 +5,9 @@
 # Creator - Rabijit
 # Date - 19/04/2026
 ##########################################
-set -x
+set -x #debug mode
+set -e #exit if error
+set -o #pipeline error
+
 ps -ef | grep "sshd" | awk -F" " '{print $2}'
 # it will find all processes with ps -ef,, grep will find a specific line(row) or statement and awk will print the column
