a8bd71837313e24e8858b8191af392215748f89b
PIN_NAKA <153271169+CHANDRAHAS-SHIVA@users.noreply.github.com>
2026-04-21 20:24:14 +0530

    Update prompt message for grep input

--- findprocessid.sh
+++ findprocessid.sh
@@ -8,7 +8,7 @@
 #set -x #debug mode
 #set -e #exit if error
 #set -o #pipeline error
-read -p "What to find" grepinput
+read -p "What to find : " grepinput
 
 ps -ef | grep "$grepinput" | awk -F" " '{print $2}'
 # it will find all processes with ps -ef,, grep will find a specific line(row) or statement and awk will print the column
