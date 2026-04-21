960c7f4e7fede69e222d12c0f384dbff62d755f3
PIN_NAKA <153271169+CHANDRAHAS-SHIVA@users.noreply.github.com>
2026-04-21 20:49:47 +0530

    Enhance findprocessid.sh for interactive process search
    Modified script to allow continuous searching for processes until 'exit' is typed. Added user prompts for input and line selection.

--- findprocessid.sh
+++ findprocessid.sh
@@ -8,7 +8,26 @@
 #set -x #debug mode
 #set -e #exit if error
 #set -o #pipeline error
-read -p "What to find : " grepinput
 
-ps -ef | grep "$grepinput" | awk -F" " '{print $2}'
+while true
+do
+        read -p "What to find (or type exit): " grepinput
+
+    # Exit condition
+    if [[ "$grepinput" == "exit" ]]; then
+        echo "Exiting..."
+        break
+    fi
+
+echo " 1 = UID , 2 = PID , 3 = PPID , 5 = STIME , 6 = TTY(Terminal) , 8 = CMD "
+
+read -p "Which line : " line
+
+ps -ef | grep "$grepinput" | grep -v grep | awk "{print \$$line}"
+
+done
+
+#ps -ef | grep "$grepinput" | awk -F" " '{print $2}'
+
 # it will find all processes with ps -ef,, grep will find a specific line(row) or statement and awk will print the column
+~
