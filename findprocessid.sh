2e8122145e5de47b5c922bb3ef1a82c3131580be
PIN_NAKA <153271169+CHANDRAHAS-SHIVA@users.noreply.github.com>
2026-04-21 20:22:39 +0530

    Update and rename findprocessdetail.sh to findprocessid.sh

--- findprocessid.sh
+++ findprocessid.sh
@@ -5,9 +5,10 @@
 # Creator - Rabijit
 # Date - 19/04/2026
 ##########################################
-set -x #debug mode
-set -e #exit if error
-set -o #pipeline error
+#set -x #debug mode
+#set -e #exit if error
+#set -o #pipeline error
+read -p "What to find" grepinput
 
-ps -ef | grep "sshd" | awk -F" " '{print $2}'
+ps -ef | grep "$grepinput" | awk -F" " '{print $2}'
 # it will find all processes with ps -ef,, grep will find a specific line(row) or statement and awk will print the column
